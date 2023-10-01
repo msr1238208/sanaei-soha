7ce974b6fc65b424975288ed534fd299e5d15f58
mohammad <m.sanaierad@gmail.com>
2023-10-01 15:26:23 +0330

    change page login to componnent

---
+++ src/components/auth/authLogin.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="w-screen h-screen bg-gray-100">
+      <div class="w-full h-full flex flex-col items-center">
+        <div class="mb-10 mt-5">
+          <img src="../assets/soha-logo.jpg" width="150" class="md:w-52" />
+        </div>
+  
+        <div class="w-full">
+          <div class="flex flex-col mx-5 md:mx-28 lg:mx-52 xl:mx-64">
+            <label class="text-right mb-1">شماره موبایل</label>
+            <input type="tel" class="px-10 py-3 outline-none rounded-lg" v-model="phoneNumber" />
+            <p class="text-right mt-1 text-red-500 text-xs mb-7">
+              شماره موبایل خالی نمی تواند باشد
+            </p>
+            <div class="w-full flex flex-col">
+              <button type="submit" class="text-slate-100 py-3 bg-gray-400 rounded-lg" @click="submit">
+                ورود یا ثبت نام
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script>
+  import axios from "axios";
+  import { useToast } from "vue-toast-notification";
+  
+  export default {
+    data() {
+      return {
+        phoneNumber: "",
+      };
+    },
+    methods: {
+      submit() {
+        axios
+          .post("https://soha.iran.liara.run/api/v1/auth/attempt", {
+            username: this.phoneNumber,
+          })
+          .then((response) => {
+            console.log(response);
+            const $toast = useToast();
+            this.$toast.success(response.message);
+            this.$router.push('/verify')
+          })
+          .catch((error) => {
+            const $toast = useToast();
+            $toast.error(error.response.message);
+          });
+      },
+    }
+  };
+  </script>
---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <authLogin />
+</template>
+
+<script>
+import authLoginVue from '@/components/auth/authLogin.vue';
+export default {
+    components:{
+        authLoginVue
+    }
+};
+</script>
+
+<style>
+</style>
