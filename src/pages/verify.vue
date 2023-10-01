52b1ea8c2d75062af4127c738ae4f9b2ca147b9f
Mohammad Sanaeirad <m.sanaierad@gmail.com>
2023-10-01 12:21:28 +0330

    eslahe khata

--- src/pages/verify.vue
+++ src/pages/verify.vue
@@ -5,7 +5,7 @@
 
       <div class="flex space-x-5 mx-16 my-10 md:mx-28 xl:mx-72">
         <input
-          class="w-full h-full py-3 items-center justify-center text-center outline-none rounded-md border text-lg bg-amber-50 focus:bg-gray-30 focus:ring-1 "
+          class="w-full h-full py-3 items-center justify-center text-center outline-none rounded-md border text-lg bg-amber-50 focus:bg-gray-30 focus:ring-1"
           type="text" v-for="i in 4" v-model="digits[i - 1]" :key="i" maxlength="1" v-on:keyup="onTextKey" />
       </div>
       <div class="w-full flex flex-col">
@@ -62,24 +62,24 @@ export default {
         }
       }
     },
-    submitVerify() {
+    submitVerify(code, username) {
       axios
-      this.userinfo.username = this.username;
-      this.userinfo.code = this.code;
         .post("https://soha.iran.liara.run/api/v1/auth/verify", {
-      })
-      .then((response) => {
-        console.log(response);
-        const $toast = useToast();
-        this.$toast.success(response.message);
-        this.$router.push('/dong')
-      })
-      .catch((error) => {
-        const $toast = useToast();
-        $toast.error(error.response.message);
-      });
-  }
-},
+          username: this.username,
+          code: this.code,
+        })
+        .then((response) => {
+          console.log(response);
+          const $toast = useToast();
+          this.$toast.success(response.message);
+          this.$router.push('/dong')
+        })
+        .catch((error) => {
+          const $toast = useToast();
+          $toast.error(error.response.message);
+        })
+
+    },
+  },
 };
 </script>
-
